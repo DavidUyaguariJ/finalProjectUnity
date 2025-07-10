7fa5b5c946ba8604e738be17626fbb678f0a5713
Galo Guevara <galo.guevara.torres@udla.edu.ec>
2025-07-09 19:56:11 -0500

    feat: movimiento y gravedad basic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,26 +1,64 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class Player : MonoBehaviour
 {
-    private float _gravity = -9.81f;
-    public float velocidad = 30f;
-    [SerializeField] private float gravityMultiplier = 3.0f;
+    [Header("Movimiento")]
+    public float moveSpeed = 5f;
+
+    [Header("Salto")]
+    public float jumpForce = 7f;
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
+    private Rigidbody rb;
+    private bool isGrounded = false;
 
     void Start()
     {
-        GetComponent<Renderer>().material.color = Color.black;
+        rb = GetComponent<Rigidbody>();
     }
-
 
     void Update()
     {
-        float movimientoX = Input.GetAxis("Horizontal");
-        float movimientoZ = Input.GetAxis("Vertical");
+        // Movimiento WASD
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+        Vector3 move = new Vector3(moveX, 0f, moveZ) * moveSpeed;
 
-        Vector3 movimiento = new Vector3(movimientoX, 0f, movimientoZ);
+        Vector3 velocity = rb.velocity;
+        velocity.x = move.x;
+        velocity.z = move.z;
+        rb.velocity = velocity;
 
-        transform.Translate(movimiento * velocidad * Time.deltaTime);
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+
+        // Aumentar gravedad si está cayendo o suelta salto
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity += Vector3.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Verifica si está tocando el suelo (por el ángulo)
+        if (collision.contacts[0].normal.y > 0.5f)
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
     }
 }
